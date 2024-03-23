a0b8b852bc3c76931f742f6967e65d1892a9fb9f
haseebahmed248 <haseebahmed6621@gmail.com>
2024-03-23 15:08:35 +0500

    Initial setup of backend with express|Cors|Routes

---
+++ BackEnd/Routes/router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Add middleware to parse JSON bodies
+router.use(express.json());
+
+router.post('/sign-up', (req, res) => {
+    const { username, email, password } = req.body;
+
+    console.log(username + " | " + email + " | " + password);
+
+    res.send('Received');
+});
+
+module.exports = router;
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import InputField from './Components/InputField'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import InputField from './Components/InputField';
+import axios from 'axios';
 
 function SignUp() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const signupData = {
+        username: username,
+        email: email,
+        password: password
+      };
+      await axios.post('http://localhost:4000/sign-up', signupData);
+      console.log("Signup successful!");
+    } catch (error) {
+      console.error("Error signing up:", error);
+      
+    }
+  };
+
   return (
     <div className='container'>
-    <h1>Sign-Up</h1>
-      <InputField type="text" placeholder="Enter your username" />
-      
-      <InputField type="text" placeholder="Enter your Email" />
-      
-      <InputField type="password" placeholder="Enter your password" />
-      
-      <h4>Continue with google</h4>
-      
-      <p>Click here to <Link to='/'>Login</Link></p>
-      <button className="signUp-BTN">Sign-Up</button>
+      <h1>Sign-Up</h1>
+      <form onSubmit={handleSubmit}>
+        <InputField type="text" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <InputField type="text" placeholder="Enter your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <InputField type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <h4>Continue with google</h4>
+        <p>Click here to <Link to='/'>Login</Link></p>
+        <button type="submit" className="signUp-BTN">Sign-Up</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
