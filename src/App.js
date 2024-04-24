73cc5b4e2c49998605178aaaa54542380263cbb9
AimanZahid1 <aimanzahid657@gmail.com>
2024-04-24 10:17:11 +0500

    atleast tried to solve useCOntext issue

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Organizations from './Pages/Organizations';
 import Home from './Pages/Main/Home';
 import AdminDashboard from './Pages/Main/AdminDashboard';
-
+import { UserProvider } from './Pages/Components/UserContext';
 
 
 function App() {
   return (
+    <UserProvider>
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
+          
+          <Route path="/signUp" element={<SignUp />}/> 
           <Route path='/admin' element={<AdminLogin />} />  
           <Route path="/organizations" element={<Organizations />} />
           <Route path="/organizations/Home" element={<Home />} />
@@ -24,6 +26,7 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
+    </UserProvider>
   );
 }
 
--- src/Pages/Components/J_AppBar.js
+++ src/Pages/Components/J_AppBar.js
@@ -1,6 +1,186 @@
-// import * as React from 'react';
-// import AppBar from '@mui/material/AppBar';
 
+// // import React, { useState } from 'react';
+// // import AppBar from '@mui/material/AppBar';
+// // import Box from '@mui/material/Box';
+// // import Toolbar from '@mui/material/Toolbar';
+// // import IconButton from '@mui/material/IconButton';
+// // import Typography from '@mui/material/Typography';
+// // import Menu from '@mui/material/Menu';
+// // import MenuIcon from '@mui/icons-material/Menu';
+// // import Container from '@mui/material/Container';
+// // import Avatar from '@mui/material/Avatar';
+// // import Button from '@mui/material/Button';
+// // import Tooltip from '@mui/material/Tooltip';
+// // import MenuItem from '@mui/material/MenuItem';
+// // import AdbIcon from '@mui/icons-material/Adb';
+// // import ProfileComponent from './Profile';
+
+// // const pages = ['Products', 'Pricing', 'Blog'];
+// // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+// // function J_AppBar() {
+// //   const [anchorElNav, setAnchorElNav] = useState(null);
+// //   const [anchorElUser, setAnchorElUser] = useState(null);
+// //   const [profileOpen, setProfileOpen] = useState(false); 
+// //   const [userId, setUserId] = useState(null); 
+
+// //   const handleOpenNavMenu = (event) => {
+// //     setAnchorElNav(event.currentTarget);
+// //   };
+
+// //   const handleOpenUserMenu = (event) => {
+// //     setAnchorElUser(event.currentTarget);
+// //   };
+
+// //   const handleCloseNavMenu = () => {
+// //     setAnchorElNav(null);
+// //   };
+
+// //   const handleCloseUserMenu = () => {
+// //     setAnchorElUser(null);
+// //   };
+
+// //   const handleProfileClick = () => {
+// //     setProfileOpen(true);
+// //     handleCloseUserMenu();
+// //   };
+
+// //   const handleCloseProfile = () => {
+// //     setProfileOpen(false);
+// //     setUserId(null); // Reset user ID when closing profile
+// //   };
+
+// //   return (
+// //     <AppBar position="static" sx={{ bgcolor: "white" }} variant='outlined'>
+// //       <Container maxWidth="l">
+// //         <Toolbar disableGutters>
+// //           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+// //           <Typography
+// //             variant="h6"
+// //             noWrap
+// //             component="a"
+// //             href="#app-bar-with-responsive-menu"
+// //             sx={{
+// //               mr: 2,
+// //               display: { xs: 'none', md: 'flex' },
+// //               fontFamily: 'monospace',
+// //               fontWeight: 700,
+// //               letterSpacing: '.3rem',
+// //               color: 'primary',
+// //               textDecoration: 'none',
+// //             }}
+// //           >
+// //             Enhanced Jira
+// //           </Typography>
+
+// //           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+// //             <IconButton
+// //               size="large"
+// //               aria-label="account of current user"
+// //               aria-controls="menu-appbar"
+// //               aria-haspopup="true"
+// //               onClick={handleOpenNavMenu}
+// //               color="primary"
+// //             >
+// //               <MenuIcon />
+// //             </IconButton>
+// //             <Menu
+// //               id="menu-appbar"
+// //               anchorEl={anchorElNav}
+// //               anchorOrigin={{
+// //                 vertical: 'bottom',
+// //                 horizontal: 'left',
+// //               }}
+// //               keepMounted
+// //               transformOrigin={{
+// //                 vertical: 'top',
+// //                 horizontal: 'left',
+// //               }}
+// //               open={Boolean(anchorElNav)}
+// //               onClose={handleCloseNavMenu}
+// //               sx={{
+// //                 display: { xs: 'block', md: 'none' },
+// //               }}
+// //             >
+// //               {pages.map((page) => (
+// //                 <MenuItem key={page} onClick={handleCloseNavMenu}>
+// //                   <Typography textAlign="center">{page}</Typography>
+// //                 </MenuItem>
+// //               ))}
+// //             </Menu>
+// //           </Box>
+// //           <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+// //           <Typography
+// //             variant="h5"
+// //             noWrap
+// //             component="a"
+// //             href="#app-bar-with-responsive-menu"
+// //             sx={{
+// //               mr: 2,
+// //               display: { xs: 'flex', md: 'none' },
+// //               flexGrow: 1,
+// //               fontFamily: 'monospace',
+// //               fontWeight: 700,
+// //               letterSpacing: '.3rem',
+// //               color: 'inherit',
+// //               textDecoration: 'none',
+// //             }}
+// //           >
+// //             LOGO
+// //           </Typography>
+// //           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+// //             {pages.map((page) => (
+// //               <Button
+// //                 key={page}
+// //                 onClick={handleCloseNavMenu}
+// //                 sx={{ my: 1, color: 'black', display: 'block', marginTop: '1%' }}
+// //               >
+// //                 {page}
+// //               </Button>
+// //             ))}
+// //           </Box>
+
+// //           <Box sx={{ flexGrow: 0 }}>
+// //             <Tooltip title="Open settings">
+// //               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+// //                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+// //               </IconButton>
+// //             </Tooltip>
+// //             <Menu
+// //               sx={{ mt: '45px' }}
+// //               id="menu-appbar"
+// //               anchorEl={anchorElUser}
+// //               anchorOrigin={{
+// //                 vertical: 'top',
+// //                 horizontal: 'right',
+// //               }}
+// //               keepMounted
+// //               transformOrigin={{
+// //                 vertical: 'top',
+// //                 horizontal: 'right',
+// //               }}
+// //               open={Boolean(anchorElUser)}
+// //               onClose={handleCloseUserMenu}
+// //             >
+// //               {settings.map((setting) => (
+// //                 <MenuItem key={setting} onClick={setting === 'Profile' ? handleProfileClick : handleCloseUserMenu}>
+// //                   <Typography textAlign="center">{setting}</Typography>
+// //                 </MenuItem>
+// //               ))}
+// //             </Menu>
+// //           </Box>
+// //         </Toolbar>
+// //       </Container>
+// //       {profileOpen && <ProfileComponent onClose={handleCloseProfile} userId={userId} />}
+// //     </AppBar>
+// //   );
+// // }
+
+// // export default J_AppBar;
+
+
+// import React, { useState, useContext, useEffect } from 'react';
+// import AppBar from '@mui/material/AppBar';
 // import Box from '@mui/material/Box';
 // import Toolbar from '@mui/material/Toolbar';
 // import IconButton from '@mui/material/IconButton';
@@ -14,17 +194,21 @@
 // import MenuItem from '@mui/material/MenuItem';
 // import AdbIcon from '@mui/icons-material/Adb';
 // import ProfileComponent from './Profile';
+// import UserContext from './UserContext'; // Import the UserContext
+
 // const pages = ['Products', 'Pricing', 'Blog'];
 // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 // function J_AppBar() {
-//   const [anchorElNav, setAnchorElNav] = React.useState(null);
-//   const [anchorElUser, setAnchorElUser] = React.useState(null);
-//   const [profileOpen, setProfileOpen] = React.useState(false); // Step 2: State to control profile component visibility
-//   const [userId, setUserId] = React.useState(null); // State to store user ID
+//   const [anchorElNav, setAnchorElNav] = useState(null);
+//   const [anchorElUser, setAnchorElUser] = useState(null);
+//   const [profileOpen, setProfileOpen] = useState(false); 
+//   const { userId } = useContext(UserContext); // Access userId from the context
+
 //   const handleOpenNavMenu = (event) => {
 //     setAnchorElNav(event.currentTarget);
 //   };
+
 //   const handleOpenUserMenu = (event) => {
 //     setAnchorElUser(event.currentTarget);
 //   };
@@ -36,19 +220,18 @@
 //   const handleCloseUserMenu = () => {
 //     setAnchorElUser(null);
 //   };
+
 //   const handleProfileClick = () => {
-//     setUserId( /* set the user ID here */ );
 //     setProfileOpen(true);
 //     handleCloseUserMenu();
 //   };
 
 //   const handleCloseProfile = () => {
 //     setProfileOpen(false);
-//     setUserId(null); // Reset user ID when closing profile
 //   };
-  
+
 //   return (
-//     <AppBar position="static" sx={{bgcolor:"white"}} variant='outlined'>
+//     <AppBar position="static" sx={{ bgcolor: "white" }} variant='outlined'>
 //       <Container maxWidth="l">
 //         <Toolbar disableGutters>
 //           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
@@ -130,7 +313,7 @@
 //               <Button
 //                 key={page}
 //                 onClick={handleCloseNavMenu}
-//                 sx={{ my: 1, color: 'black', display: 'block',marginTop:'1%' }}
+//                 sx={{ my: 1, color: 'black', display: 'block', marginTop: '1%' }}
 //               >
 //                 {page}
 //               </Button>
@@ -160,8 +343,7 @@
 //               onClose={handleCloseUserMenu}
 //             >
 //               {settings.map((setting) => (
-               
-// <MenuItem key={setting} onClick={setting === 'Profile' ? handleProfileClick : handleCloseUserMenu}>
+//                 <MenuItem key={setting} onClick={setting === 'Profile' ? handleProfileClick : handleCloseUserMenu}>
 //                   <Typography textAlign="center">{setting}</Typography>
 //                 </MenuItem>
 //               ))}
@@ -169,14 +351,14 @@
 //           </Box>
 //         </Toolbar>
 //       </Container>
-//       {profileOpen && <ProfileComponent onClose={handleCloseProfile} userId={userId} />} {/* Pass userId to ProfileComponent */}
+//       {profileOpen && <ProfileComponent onClose={handleCloseProfile} userId={userId} />}
 //     </AppBar>
 //   );
 // }
+
 // export default J_AppBar;
 
-
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -191,6 +373,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import ProfileComponent from './Profile';
+import UserContext from './UserContext'; // Import the UserContext
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -198,8 +381,8 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 function J_AppBar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const [profileOpen, setProfileOpen] = useState(false); // Step 2: State to control profile component visibility
-  const [userId, setUserId] = useState(null); // State to store user ID
+  const [profileOpen, setProfileOpen] = useState(false);
+  const { userId } = useContext(UserContext); // Access userId from the context
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -218,13 +401,13 @@ function J_AppBar() {
   };
 
   const handleProfileClick = () => {
+    console.log(userId)
     setProfileOpen(true);
     handleCloseUserMenu();
   };
 
   const handleCloseProfile = () => {
     setProfileOpen(false);
-    setUserId(null); // Reset user ID when closing profile
   };
 
   return (
