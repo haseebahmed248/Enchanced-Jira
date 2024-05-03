75c1ea02e26586c9092cd5ac34a660f09b71633e
AimanZahid1 <aimanzahid657@gmail.com>
2024-05-03 16:11:34 +0500

    BackEnd of Organiztion issues

--- BackEnd/BackEnd_Organizations/server.js
+++ BackEnd/BackEnd_Organizations/server.js
@@ -1,17 +1,21 @@
 const bodyParser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
-const organizationRouter = require('./routes/organization');
-
+const express = require('express')
+const cors = require('cors')
+const organization = require('./Routes/organization')
+const dotenv = require('dotenv')
+dotenv.config()
+const {corsConfig} = require('./Controller/serveController')
 const app = express();
-const PORT = process.env.PORT || 3000;
+dotenv.config()
+const PORT = process.env.PORT;
+app.use(cors(corsConfig))
+app.use(bodyParser.json())
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/organization', organizationRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+
+app.use('/organization',organization)
+
+
+app.listen(PORT,()=>{
+    console.log(`Servier is Listening to Port ${PORT}`);
+})
